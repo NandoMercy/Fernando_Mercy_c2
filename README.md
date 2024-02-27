53f898626d88535c5c9ce2d8781c2a1067422b26
Fernando Mercy <nando.mercy@gmail.com>
2024-02-27 17:37:43 -0300

    componente card y form agregado

--- README.md
+++
@@ -1,1 +0,0 @@
-# examen-parcial-react
---
+++ parcial-front-3/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Card from './Components/Card'
+import Form from './Components/Form'
+
+function App() {
+
+
+  return (
+<>
+<div className="App">
+  <h1>Carga de estudiantes</h1>
+  <Form/>
+  <Card/>
+</div>
+</>
+
+  )
+}
+
+export default App
